7315751c6b189f86bddbb9584f764c3fc7a67a92
Nicholas Pringle <nicholaschristopherpringle@gmail.com>
2016-07-30 19:26:18 +0200

    make API endpoints for PostViewSet and AccountPostViewSet

--- foyer/urls.py
+++ foyer/urls.py
@@ -19,13 +19,22 @@ from rest_framework_nested import routers
 
 from .views import IndexView
 from authentication.views import AccountViewSet, LoginView, LogoutView
+from posts.views import AccountPostsViewSet, PostViewSet
+
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
+router.register(r'posts', PostViewSet)
+
+accounts_router = routers.NestedSimpleRouter(
+    router, r'accounts', lookup='account'
+)
+accounts_router.register(r'posts', AccountPostsViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(accounts_router.urls)),
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url('^.*$', IndexView.as_view(), name='index'),
