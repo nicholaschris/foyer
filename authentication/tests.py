18a6521f4539fb471a7213bb88d84e5a132e14d9
Nicholas Pringle <nicholaschristopherpringle@gmail.com>
2016-09-21 16:36:39 +0200

    Setup first django unit tests

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+User = get_user_model()
+
+class UserModelTest(TestCase):
+
+    def test_user_is_valid_with_email_and_password_and_username(self):
+        user = User(email="a@b.com", password="abc", username="a")
+        user.full_clean()
+
+    def test_is_authenticated(self):
+        user = User()
+        self.assertTrue(user.is_authenticated())
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from posts.models import Post
 # Create your tests here.
+
+class PostModelsTest(TestCase):
+
+    def test_one_plus_one(self):
+        self.assertEqual(1 + 1, 2) 
